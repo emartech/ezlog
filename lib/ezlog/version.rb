8a533be9050223018ee1ffc9f85bd4de75e9710a
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2019-05-03 11:10:28 +0200

    Gem generated by Bundler

---
+++ lib/ezlog/version.rb
@@ -0,0 +1,3 @@
+module Ezlog
+  VERSION = "0.1.0"
+end
---
+++ spec/ezlog_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ezlog do
+  it "has a version number" do
+    expect(Ezlog::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
