c58b83bb4837417a98a0c62b0291a652918397db
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2019-05-11 10:56:29 +0200

    Refactoring

--- spec/ezlog/sidekiq/job_logger_spec.rb
+++ spec/ezlog/sidekiq/job_logger_spec.rb
@@ -64,17 +64,17 @@ RSpec.describe Ezlog::Sidekiq::JobLogger do
     end
 
     context 'when the job itself logs a message' do
-      it "includes the context information of the job" do
-        expect do
-          job_logger.call(item, queue) { Sidekiq.logger.info 'Message during processing' }
-        end.to log(message: 'Message during processing',
-                   jid: 'job id',
-                   queue: 'job queue',
-                   worker: 'TestWorker',
-                   customer_id: 1,
-                   name: 'name param',
-                   created_at: now,
-                   enqueued_at: now).at_level(:info)
+      let(:call_with_logging) { job_logger.call(item, queue) { Sidekiq.logger.info 'Message during processing' } }
+
+      it 'includes the context information of the job' do
+        expect { call_with_logging }.to log(message: 'Message during processing',
+                                            jid: 'job id',
+                                            queue: 'job queue',
+                                            worker: 'TestWorker',
+                                            customer_id: 1,
+                                            name: 'name param',
+                                            created_at: now,
+                                            enqueued_at: now).at_level(:info)
       end
     end
 
@@ -87,20 +87,20 @@ RSpec.describe Ezlog::Sidekiq::JobLogger do
     end
 
     context 'when there is an error processing the job' do
+      let(:call_with_exception) { job_logger.call(item, queue) { raise Exception } }
+
       it 'lets the error through and logs a failure message with timing' do
         allow(::Process).to receive(:clock_gettime).with(Process::CLOCK_MONOTONIC).and_return(1.0, 2.0)
-        expect { job_logger.call(item, queue) { raise Exception } }.to raise_error(Exception)
-                                                                         .and log(message: 'TestWorker failed',
-                                                                                  jid: 'job id',
-                                                                                  queue: 'job queue',
-                                                                                  worker: 'TestWorker',
-                                                                                  customer_id: 1,
-                                                                                  name: 'name param',
-                                                                                  created_at: now,
-                                                                                  enqueued_at: now,
-                                                                                  duration_sec: 1.0).at_level(:info)
-
-
+        expect { call_with_exception }.to raise_error(Exception)
+                                            .and log(message: 'TestWorker failed',
+                                                     jid: 'job id',
+                                                     queue: 'job queue',
+                                                     worker: 'TestWorker',
+                                                     customer_id: 1,
+                                                     name: 'name param',
+                                                     created_at: now,
+                                                     enqueued_at: now,
+                                                     duration_sec: 1.0).at_level(:info)
       end
     end
   end
