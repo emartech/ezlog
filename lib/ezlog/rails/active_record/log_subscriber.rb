4838cf7327fb1bf94ca537fa61b10eb7eb95701f
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2019-08-16 14:15:02 +0200

    Log ActiveRecord query parameters

--- lib/ezlog/rails/active_record/log_subscriber.rb
+++ lib/ezlog/rails/active_record/log_subscriber.rb
@@ -3,9 +3,33 @@ module Ezlog
     module ActiveRecord
       class LogSubscriber < ::ActiveSupport::LogSubscriber
         def sql(event)
-          ::ActiveRecord::Base.logger.debug message: "SQL - #{event.payload[:name]} (#{event.duration.round(3)}ms)",
-                                            sql: event.payload[:sql],
-                                            duration_sec: (event.duration / 1000.0).round(5)
+          ::ActiveRecord::Base.logger.debug log_message_from(event)
+        end
+
+        private
+
+        def log_message_from(event)
+          basic_message_from(event).tap do |message|
+            params = params_from event
+            message[:params] = params if params.present?
+          end
+        end
+
+        def basic_message_from(event)
+          {
+            message: "SQL - #{event.payload[:name]} (#{event.duration.round(3)}ms)",
+            sql: event.payload[:sql],
+            duration_sec: (event.duration / 1000.0).round(5)
+          }
+        end
+
+        def params_from(event)
+          return if event.payload.fetch(:binds, []).empty?
+
+          names = event.payload[:binds].map(&:name)
+          values = event.payload[:type_casted_binds]
+
+          Hash[names.zip(values)]
         end
       end
     end
--- spec/ezlog/rails/active_record/log_subscriber_spec.rb
+++ spec/ezlog/rails/active_record/log_subscriber_spec.rb
@@ -20,5 +20,41 @@ RSpec.describe Ezlog::Rails::ActiveRecord::LogSubscriber do
                                       sql: 'SELECT * FROM users',
                                       duration_sec: 0.00124).at_level(:debug)
     end
+
+    context 'when the query has parameters' do
+      let(:event) do
+        instance_double ActiveSupport::Notifications::Event,
+                        payload: {
+                          name: 'User Load',
+                          sql: 'SELECT * FROM users LIMIT $1',
+                          binds: [instance_double(ActiveRecord::Relation::QueryAttribute, name: 'LIMIT', value: 1)],
+                          type_casted_binds: [1],
+                          type: instance_double(ActiveModel::Type::Value, binary?: false)
+                        },
+                        duration: 1.235
+      end
+
+      it 'logs the query parameters' do
+        expect { trigger_event }.to log(params: {LIMIT: 1}).at_level(:debug)
+      end
+
+      xcontext 'when there are binary parameters' do
+        let(:event) do
+          instance_double ActiveSupport::Notifications::Event,
+                          payload: {
+                            name: 'User Load',
+                            sql: 'SELECT * FROM users WHERE bytecode = $1',
+                            binds: [instance_double(ActiveRecord::Relation::QueryAttribute, name: 'bytecode', value: 'some binary value')],
+                            type_casted_binds: ['some binary value'],
+                            type: instance_double(ActiveModel::Type::Value, binary?: true)
+                          },
+                          duration: 1.235
+        end
+
+        it "doesn't log the binary parameter's value" do
+          expect { trigger_event }.to log(bytecode: '<binary data>').at_level(:debug)
+        end
+      end
+    end
   end
 end
