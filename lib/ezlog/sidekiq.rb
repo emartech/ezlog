950aac265a08aae2122a30c7d13f52a199a6f3c9
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2020-04-07 08:27:17 +0200

    Revert "Make sure Sidekiq's logger conforms to the new Sidekiq 6 logger interface" because it is not necessary and causes problems with sidekiq-unique-jobs

--- lib/ezlog/sidekiq.rb
+++ lib/ezlog/sidekiq.rb
@@ -3,6 +3,5 @@ module Ezlog
     autoload :ErrorLogger, 'ezlog/sidekiq/error_logger'
     autoload :JobContext, 'ezlog/sidekiq/job_context'
     autoload :JobLogger, 'ezlog/sidekiq/job_logger'
-    autoload :LoggerExtension, 'ezlog/sidekiq/logger_extension'
   end
 end
